02ee2438f3e3ad23092eb0406c8cb5034ed2c6eb
Hariharan Jayashankar <hariharan.jayashankar@gmail.com>
2023-05-23 18:36:06 -0400

    added help

--- invertpdf.sh
+++ invertpdf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [[ "$1" == "-h" ]]; then
+  echo "Used to invert pdf colors. First argument is the input, second argument is the output."
+  exit 0
+fi
+
+
 convert -density 300 -colorspace RGB -channel RGB -negate $1 $2
