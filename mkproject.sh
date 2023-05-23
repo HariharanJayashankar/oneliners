fc44f2b01c345a8d62d672875b61456d16385730
Hariharan Jayashankar <hariharan.jayashankar@gmail.com>
2023-05-23 18:50:36 -0400

    added project maker

---
+++ mkproject.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ "$1" == "-h" ]]; then
+  echo "Initiate folder structure for project."
+  echo "Ideal Usecase:"
+  echo "Raw directory contains raw untouched data"
+  echo "Processed directory contains files from raw which are processed for use"
+  echo "Output directory contains output like tables and figures"
+  echo "Documentation contains documentation"
+  echo "code/cleaning contains code which cleans data in raw and saves in processed"
+  echo "code/analysis contains code which analyzes data in either raw or processed and outputs to output"
+  exit 0
+fi
+
+mkdir -p raw
+mkdir -p output
+mkdir -p documentation
+mkdir -p processed
+mkdir -p code/{cleaning, analysis}
+touch readme
